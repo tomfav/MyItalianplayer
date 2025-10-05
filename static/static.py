b78a7a0aab88330381242c88e682dc5da3cfd150
tomfav <79805838+tomfav@users.noreply.github.com>
2025-10-05 16:17:37 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,3 +1,6 @@
 HTML = """
-This is a random text that nobody cares about 1234567890
+This is a random text that nobody cares text about
+random This is a random text text that nobody cares about
+This is a random text that nobody cares about
+random This text is a random text that nobody cares about
 """
